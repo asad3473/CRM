5cd6fa7a53c104090c81472332063f9bd85db60b
asad3473 <asadsoomro0311@gmail.com>
2025-09-15 12:08:45 +0500

    dashboard setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
-import './app.css'
+// import './app.css'
 import SideBar from './AdminDashboard/Pages/SideBar'
 import HQoverview from './AdminDashboard/Components/FeedBack/HQoverview'
 import BranchPerformance from './AdminDashboard/Components/FeedBack/BranchPerformance'
